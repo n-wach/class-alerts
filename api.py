73f7f716512433904a71605b1f29ff905f64b3ce
Nathan <nmwachholz@gmail.com>
2020-04-01 14:15:59 -0700

    Fixed honeypot to sign-up page

--- api.py
+++ api.py
@@ -82,7 +82,7 @@ def route(app):
                 send_verification_email(user)
                 return redirect(url_for("verify_email"))
         else:
-            logger.info("Honeypot detected spam from {} with subject '{}'".format(email, subject))
+            logger.info("Honeypot detected spam from in sign-up for {}".format(create_email))
 
 
     @app.route("/api/user/verify-email/<code>")
